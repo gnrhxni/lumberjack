0fe163eed3f00bee8bedc5335da70819c7deb58f
gnrhxni <gnrhxni@outlook.com>
2013-05-04 21:52:00 -0400

    Generally works. Version bump time.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='lumberjack',
-    version='0.0.1',
+    version='0.1.0',
     description='Real time file streaming over HTTP',
     packages=find_packages(exclude=['ez_setup', 'tests', 'tests.*']),
     zip_safe=False,
